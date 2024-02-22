a949bce202bd9ad9bbec0bc07c8c17cea58be9aa
tastevision <xianghuang@my.swjtu.edu.cn>
2024-02-22 22:00:12 +0800

    update training script

--- run_train_full.sh
+++ run_train_full.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-# two branches
+# two branches (same model)
 . ./script/DAMO_s_DAMO_m.sh
 . ./script/DAMO_s_DAMO_l.sh
 . ./script/DAMO_m_DAMO_l.sh
@@ -9,6 +9,13 @@
 . ./script/SYOLO_s_SYOLO_m.sh
 . ./script/SYOLO_s_SYOLO_l.sh
 . ./script/SYOLO_m_SYOLO_l.sh
+
+# two branches (different model)
+. ./script/DAMO_s_LSN_s.sh
+. ./script/DAMO_s_LSN_m.sh
+. ./script/DAMO_s_LSN_l.sh
+. ./script/DAMO_m_LSN_s.sh
+. ./script/DAMO_l_LSN_s.sh
 
 # three branches
 . ./script/DAMO_s_DAMO_m_DAMO_l.sh
