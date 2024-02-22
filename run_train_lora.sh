3586c74ab8ab92a1f3acc254e717e7d49ec5a458
tastevision <xianghuang@my.swjtu.edu.cn>
2024-02-22 21:54:13 +0800

    update training script

--- run_train_lora.sh
+++ run_train_lora.sh
@@ -1,49 +1,16 @@
 #!/bin/bash
+# two branches
+. ./script/DAMO_s_DAMO_m_lora.sh
+. ./script/DAMO_s_DAMO_l_lora.sh
+. ./script/DAMO_m_DAMO_l_lora.sh
+. ./script/LSN_s_LSN_m_lora.sh
+. ./script/LSN_s_LSN_l_lora.sh
+. ./script/LSN_m_LSN_l_lora.sh
+. ./script/SYOLO_s_SYOLO_m_lora.sh
+. ./script/SYOLO_s_SYOLO_l_lora.sh
+. ./script/SYOLO_m_SYOLO_l_lora.sh
 
-# # Full finetune
-# . ./script/DAMO_s_DAMO_m.sh
-# . ./script/DAMO_s_DAMO_l.sh
-# . ./script/DAMO_m_DAMO_l.sh
-# . ./script/LSN_s_LSN_m.sh
-# . ./script/LSN_s_LSN_l.sh
-# . ./script/LSN_m_LSN_l.sh
-# . ./script/SYOLO_s_SYOLO_m.sh
-# . ./script/SYOLO_s_SYOLO_l.sh
-# . ./script/SYOLO_m_SYOLO_l.sh
-
-# # LoRA finetune
-# . ./script/DAMO_s_DAMO_m_lora.sh
-# . ./script/DAMO_s_DAMO_l_lora.sh
-# . ./script/DAMO_m_DAMO_l_lora.sh
-# . ./script/LSN_s_LSN_m_lora.sh
-# . ./script/LSN_s_LSN_l_lora.sh
-# . ./script/LSN_m_LSN_l_lora.sh
-# . ./script/SYOLO_s_SYOLO_m_lora.sh
-# . ./script/SYOLO_s_SYOLO_l_lora.sh
-# . ./script/SYOLO_m_SYOLO_l_lora.sh
-
-# . ./script/DAMO_s_LSN_s_lora.sh
-# . ./script/DAMO_s_LSN_m_lora.sh
-# . ./script/DAMO_s_LSN_l_lora.sh
-# . ./script/DAMO_m_LSN_s_lora.sh
-# . ./script/DAMO_l_LSN_s_lora.sh
-
-# . ./script/DAMO_s_DAMO_s_lora.sh # yb-s7
-# . ./script/DAMO_m_DAMO_m_lora.sh # yb-s7
-# . ./script/DAMO_l_DAMO_l_lora.sh # yb-s7
-# . ./script/DAMO_s_DAMO_s.sh # yb-s7
-# . ./script/DAMO_m_DAMO_m.sh # yb-s7
-# . ./script/DAMO_l_DAMO_l.sh # yb-s7
-# . ./script/LSN_s_LSN_s_lora.sh # yb-s25
-# . ./script/LSN_m_LSN_m_lora.sh # yb-s25
-# . ./script/LSN_l_LSN_l_lora.sh # yb-s7
-# . ./script/LSN_s_LSN_s.sh # yb-s25
-# . ./script/LSN_m_LSN_m.sh # yb-s25
-# . ./script/LSN_l_LSN_l.sh # yb-s25
-
-# . ./script/SYOLO_s_SYOLO_s.sh # yb-s25
-# . ./script/SYOLO_m_SYOLO_m.sh # yb-s7
-. ./script/SYOLO_l_SYOLO_l.sh # yb-s25
-# . ./script/SYOLO_s_SYOLO_s_lora.sh # yb-s25
-# . ./script/SYOLO_m_SYOLO_m_lora.sh # yb-s7
-. ./script/SYOLO_l_SYOLO_l_lora.sh # yb-s7
+# three branches
+. ./script/DAMO_s_DAMO_m_DAMO_l_lora.sh
+. ./script/LSN_s_LSN_m_LSN_l_lora.sh
+. ./script/SYOLO_s_SYOLO_m_SYOLO_l_lora.sh
